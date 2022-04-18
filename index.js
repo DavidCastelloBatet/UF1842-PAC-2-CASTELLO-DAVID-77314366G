0b4dc908c2b242c5a06b9b7adf19acdfb085c53b
David Castello <davidcastellobatetgmail.com>
2022-04-18 19:34:57 +0200

    Expandir

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 window.onload = inicio;
 
 function inicio() {
+  var vid = document.querySelectorAll("video");
   // actualitzacion continua del tiempo - funcio actualitzarTemps
-  actualitzarTemps("1");
-  actualitzarTemps("2");
-
   // actualitzacio del temps quan carreguem el video- actualitza
   // el temps de ala durada del video i evitara el NaN
+  vid.onloadeddata = actualitzarTemps;
+  vid.ontimeupdate = actualitzarTemps;
+  actualitzarTemps(vid);
+
+  document.getElementById("reproductor1").style.transform = "scale(1.0)";
+  document.getElementById("reproductor2").style.transform = "scale(1.0)";
 }
 
 class Video {
@@ -51,23 +55,19 @@ let reproductor2 = new Video("2");
 let reproductor = new Video("video");
 
 // funciones que afectan a la BARRA DE PROGRESO
-function actualitzarTemps(numVideo) {
-  var vid1 = document.querySelector("#video1 video");
-  var vid2 = document.querySelector("#video2 video");
-
-  console.log(vid1);
-  console.log(numVideo); // el numero de video entra correcte
-  // mostramos tiempo y durada del video
-  let videoObtingut = `vid${numVideo}`;
-  console.log(videoObtingut);
-  document.querySelector(`.estado${numVideo}`).innerHTML = `${conversion(
-    videoObtingut.currentTime
-  )} / ${conversion(videoObtingut.duration)}`;
-}
+let actualitzarTemps = (vid) => {
+  for (i = 0; i < vid.length; i++) {
+    // mostramos tiempo y durada del video
+    document.getElementById("estado1").innerHTML = `${conversion(
+      vid[i].currentTime
+    )}     /    ${conversion(vid[i].duration)}`;
+  }
+};
 // amb aquesta funció transformo els milisegons que rebo per
 // parametres i retorno minuts i segons en format 00 : 00
 // per parametres rebem els milisegons de la data actual i la duració del video
-let conversion = (segundos) => {
+
+function conversion(segundos) {
   let progresoVideo = new Date(segundos * 1000);
   // si el numero de segons es menor que nou, concatenem un 0 per simular 2 digits
   let segundo =
@@ -81,4 +81,39 @@ let conversion = (segundos) => {
       : progresoVideo.getMinutes();
   // retornem la conversio milisegons en minuts i segons
   return `${minuto}:${segundo}`;
+}
+
+// funcion expandir / contraer video
+expandir1 = () => {
+  let seccio1 = document.getElementById("reproductor1");
+  let seccio2 = document.getElementById("reproductor2");
+  // Accedim a transform (propietat CSS)
+  if (seccio1.style.transform == "scale(1)") {
+    // si esta en 0.6 la canviem a 1 i modifiquem icona
+    seccio1.style.transform = "scale(1.2)";
+    seccio2.style.transform = "scale(0.5)";
+    document.querySelector(".expandir").src = "./images/reducir.svg";
+  } else {
+    seccio1.style.transform = "scale(1)";
+    seccio2.style.transform = "scale(1)";
+
+    document.querySelector(".expandir").src = "./images/expandir.svg";
+  }
+};
+expandir2 = () => {
+  let seccio1 = document.getElementById("reproductor1");
+  let seccio2 = document.getElementById("reproductor2");
+  // Accedim a transform (propietat CSS)
+  if (seccio2.style.transform == "scale(1)") {
+    // si esta en 0.6 la canviem a 1 i modifiquem icona
+    seccio2.style.transform = "scale(1.2)";
+    seccio1.style.transform = "scale(0.5)";
+
+    document.querySelector(".expandir").src = "./images/reducir.svg";
+  } else {
+    seccio2.style.transform = "scale(1)";
+    seccio1.style.transform = "scale(1)";
+
+    document.querySelector(".expandir").src = "./images/expandir.svg";
+  }
 };
